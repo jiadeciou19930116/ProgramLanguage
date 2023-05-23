a6ac9341e299e8e2359f58e9b530683e9c4df1a1
jiadeciou19930116 <jiadeciou@gmail.com>
2023-05-23 16:04:12 +0800

    Edit HelloWorld.cpp

--- C++/HelloWorld.cpp
+++ C++/HelloWorld.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main()
 {
 	cout << "Hello World, mother fucker!" << endl;
+	cout << "I'm trying push you to GitHub!";
 	return 0;
 }
---
+++ ReadMe.txt
@@ -0,0 +1,3 @@
+這個資料夾裡會存放我寫的範例程式
+Here would store the example processes when I learn and pratice coding
+現在也在本資料夾中弄了git資料庫,可以嘗試git版本控制了！
