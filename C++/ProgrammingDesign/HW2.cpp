aac007f5089dd8715abf7962ab62427a1598c090
Jiade <33255436+jiadeciou19930116@users.noreply.github.com>
2023-05-31 16:58:28 +0800

    Start to re-edit HW2.

---
+++ C++/ProgrammingDesign/HW2.cpp
@@ -0,0 +1,36 @@
+/* HW2 Practice
+ *Programming Design In-class Practices
+ *Variables and Arrays
+ */
+ #include<iostream>
+ using namespace std;
+ 
+ void problem1();
+ void problem2();
+ void problem3();
+ void problem4();
+ 
+ int main()
+ {
+ 	return 0;
+ }
+ 
+void problem1()
+{
+ 	return;
+};
+ 
+void problem2()
+{
+ 	return;
+};
+ 
+void problem3()
+{
+ 	return;
+};
+ 
+void problem4()
+{
+ 	return;
+};
