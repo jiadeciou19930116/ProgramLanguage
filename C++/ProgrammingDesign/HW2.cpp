99a939683dc198d2cf831e68c0b141a095f5bba8
Jiade <33255436+jiadeciou19930116@users.noreply.github.com>
2023-06-06 10:01:32 +0800

    complete HW2_2

--- C++/ProgrammingDesign/HW2.cpp
+++ C++/ProgrammingDesign/HW2.cpp
@@ -28,11 +28,14 @@
 			case 1:
 				problem1();
 				break;
+		case 2:
+				problem2();
+				break;
 			default:
 				cout << "Out of range, please input again.";
 				break;
 		}
-		cout << endl;
+		
 	}while(proIndex);
 	
  	return 0;
@@ -52,6 +55,60 @@ void problem1()
  
 void problem2()
 {
+	/*Problem2: the knapsack problem
+	*/
+	int carry = 0;
+	int numofItem = 0;
+	int totalWeight = 0;
+	int totalValue = 0;
+	
+	cout << "Please input the number of items and bag carry: ";
+	cin >> numofItem >> carry;
+	
+	int* itemWeight = new int[numofItem];
+	int* itemValue = new int[numofItem];
+	bool* itemSelect = new bool[numofItem];
+	
+	cout << "Please input the weights of items: ";
+	for(int i = 0; i < numofItem; i++)
+	{
+		cin >> itemWeight[i];
+	}
+	
+	cout << "Please input the values of items: ";
+	for(int i = 0; i < numofItem; i++)
+	{
+		cin >> itemValue[i];
+	}
+	
+	cout << "Input 1 if the item is selected or 0 otherwise for each items: ";
+	for(int i = 0; i < numofItem; i++)
+	{
+		cin >> itemSelect[i];
+	}
+	
+	//solve total weight and value
+	for(int i = 0; i < numofItem; i++)
+	{
+		if(itemSelect[i])
+		{
+			totalWeight += itemWeight[i];
+			totalValue += itemValue[i];
+		}
+	}
+	if(totalWeight > carry)
+	{
+		cout << "Out of carry!" << endl;
+	}
+	else
+	{
+		cout << "Total weight and value are " << totalWeight << " " << totalValue << "." << endl;
+	}
+	
+	/*delete [] itemWeight;
+	delete [] itemValue;
+	delete [] itemSelect;
+	*/
  	return;
 };
  
